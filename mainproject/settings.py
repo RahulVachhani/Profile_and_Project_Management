f962ffdba1fa95c2c2b8152e4177277d8372a61e
RahulVachhani <rahul.vachhani@openxcell.com>
2024-11-21 08:18:03 +0530

    remove setting.py info

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -197,10 +197,3 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'django.learning.testing@gmail.com'
-EMAIL_HOST_PASSWORD = 'ispoojkxpnaspwyz'
