640d293652cbb397846f63e001486e330706a711
RahulVachhani <rahul.vachhani@openxcell.com>
2025-02-06 15:13:13 +0530

    add build.sh file

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 
+
+set -o errexit
+
 pip install -r requirements.txt
+python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
+if [[ $CREATE_SUPERUSER]];
+then 
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
